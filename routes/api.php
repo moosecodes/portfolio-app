467c72448b099cca9d6f530d242b1910b69943e5
moose <moosecodes@gmail.com>
2023-06-10 16:06:50 -0700

    implement weather endpoint, model, migration

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\WeatherReading;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/temp', function() {
-	return 'hello world!';
+Route::get('/weather/{zip}', function(string $zip) {
+    $response = Http::get(env('WEATHER_API_URL'), [
+        'key' => env('WEATHER_API_KEY'),
+        'q' => $zip
+    ]);
+    $reading = $response->json();
+
+    $weatherReading = new WeatherReading;
+
+    $weatherReading->temp_f = $reading['current']['temp_f'];
+    $weatherReading->temp_c = $reading['current']['temp_c'];
+    $weatherReading->city = $reading['location']['name'];
+    $weatherReading->region = $reading['location']['region'];
+    $weatherReading->save();
+
+    return $weatherReading->orderBy('id', 'DESC')->first();
 });
